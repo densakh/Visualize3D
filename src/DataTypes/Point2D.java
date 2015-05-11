5f32479135efb8b2f6b903b93ea05416d67f4eb1
denSAKH <balanev.di@gmail.com>
2015-05-11 21:35:38 +1000

    2D Convex Hull
    Jarvis algorithm

---
+++ src/DataTypes/Point2D.java
@@ -0,0 +1,25 @@
+package DataTypes;
+
+public class Point2D {
+	private double _x;
+	private double _y;
+	public Point2D(double x, double y){
+		_x = x;
+		_y = y;
+	}
+	
+	public double sqrLen(Point2D p){
+		return Math.pow(p._x - _x, 2) + Math.pow(p._y, 2);
+	}
+	
+	public double minPolarAngle(Point2D p1, Point2D p2){
+		return (p1._x - _x) * (p2._y - _y) - (p2._x - _x) * (p1._y - _y);
+	}
+	
+	public double polarAngle(Point2D p1){
+		return Math.atan2(p1._y - _y, p1._x - _x);
+	}
+	
+	public double getX(){ return _x; };
+	public double getY(){ return _y; };
+}
