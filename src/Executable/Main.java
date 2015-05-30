4b8fabadf7c24c839b8be65b39570f169430e2f5
Daniil Demidenko <danilka95v@gmail.com>
2015-05-30 16:26:57 +1000

    Test

--- src/Executable/Main.java
+++ src/Executable/Main.java
@@ -1,15 +1,30 @@
 package Executable;
 
 import Calculus.ConvexHull2D;
-import Calculus.DeloneTriangulation;
-import DataTypes.Point2D;
+
+import Calculus.Triangulation;
+
+import DataTypes.HalfEdge;
+import DataTypes.Vertex;
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBuilder;
 import javafx.scene.transform.Rotate;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.paint.Color;
@@ -19,6 +34,7 @@ import javafx.scene.PerspectiveCamera;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Random;
 
 /**
@@ -26,268 +42,129 @@ import java.util.Random;
  */
 public class Main extends Application {
 
-    final PerspectiveCamera camera = new PerspectiveCamera(true);
-    final Xform cameraXform = new Xform();
-    final Xform cameraXform2 = new Xform();
-    final Xform cameraXform3 = new Xform();
-    final Xform moleculeGroup = new Xform();
-    final double cameraDistance = 450;
-    final Group root = new Group();
-    final Xform world = new Xform();
-    final Group axisGroup = new Group();
-    private static final double CAMERA_INITIAL_DISTANCE = -450;
-    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
-    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
-    private static final double CAMERA_NEAR_CLIP = 0.1;
-    private static final double CAMERA_FAR_CLIP = 10000.0;
-    private static final double AXIS_LENGTH = 250.0;
-    private static final double HYDROGEN_ANGLE = 104.5;
-    private static final double CONTROL_MULTIPLIER = 0.1;
-    private static final double SHIFT_MULTIPLIER = 10.0;
-    private static final double MOUSE_SPEED = 0.1;
-    private static final double ROTATION_SPEED = 2.0;
-    private static final double TRACK_SPEED = 0.3;
-    double mousePosX;
-    double mousePosY;
-    double mouseOldX;
-    double mouseOldY;
-    double mouseDeltaX;
-    double mouseDeltaY;
+    private static final int VIEWPORT_SIZE = 800;
 
-    private void buildScene() {
-        System.out.println("buildScene");
-        root.getChildren().add(world);
+    private static final double MODEL_SCALE_FACTOR = 40;
+    private static final double MODEL_X_OFFSET = 0;
+    private static final double MODEL_Y_OFFSET = 0;
+    private static final double MODEL_Z_OFFSET = VIEWPORT_SIZE / 2;
+
+    //  private static final String textureLoc = "http://icons.iconarchive.com/icons/aha-soft/desktop-halloween/256/Skull-and-bones-icon.png"; // icon license linkware, backlink to http://www.aha-soft.com
+    private static final String textureLoc = "http://d366vafdki9sdu.cloudfront.net/files/37/2688/seamless-marble-textures-pack-screenshots-4.jpg";
+    // texture sourced from: http://www.creattor.com/textures-patterns/seamless-marble-textures-pack-2688
+    // texture license: http://creativecommons.org/licenses/by-nc/3.0/ => Creative Commons Attribution-NonCommercial 3.0 Unported
+
+    private Image texture;
+    private PhongMaterial texturedMaterial = new PhongMaterial();
+
+    private MeshView meshView = loadMeshView();
+
+    private MeshView loadMeshView() {
+        float[] points = {
+                -5, 5, 0,
+                -5, -5, 0,
+                5, 5, 0,
+                5, -5, 0
+        };
+        float[] texCoords = {
+                1, 1,
+                1, 0,
+                0, 1,
+                0, 0
+        };
+        int[] faces = {
+                2, 2, 1, 1, 0, 0,
+                2, 2, 3, 3, 1, 1
+        };
+
+        TriangleMesh mesh = new TriangleMesh();
+        mesh.getPoints().setAll(points);
+        mesh.getTexCoords().setAll(texCoords);
+        mesh.getFaces().setAll(faces);
+
+        return new MeshView(mesh);
     }
 
+    private Group buildScene() {
+        meshView.setTranslateX(VIEWPORT_SIZE / 2 + MODEL_X_OFFSET);
+        meshView.setTranslateY(VIEWPORT_SIZE / 2 * 9.0 / 16 + MODEL_Y_OFFSET);
+        meshView.setTranslateZ(VIEWPORT_SIZE / 2 + MODEL_Z_OFFSET);
+        meshView.setScaleX(MODEL_SCALE_FACTOR);
+        meshView.setScaleY(MODEL_SCALE_FACTOR);
+        meshView.setScaleZ(MODEL_SCALE_FACTOR);
 
-    private void buildCamera() {
-        root.getChildren().add(cameraXform);
-        cameraXform.getChildren().add(cameraXform2);
-        cameraXform2.getChildren().add(cameraXform3);
-        cameraXform3.getChildren().add(camera);
-        cameraXform3.setRotateZ(0.0);
-
-        camera.setNearClip(0.1);
-        camera.setFarClip(10000.0);
-        camera.setTranslateZ(-cameraDistance);
-        cameraXform.ry.setAngle(0.0);
-        cameraXform.rx.setAngle(0);
-    }
-
-
-    private void buildAxes() {
-        System.out.println("buildAxes()");
-        final PhongMaterial redMaterial = new PhongMaterial();
-        redMaterial.setDiffuseColor(Color.DARKRED);
-        redMaterial.setSpecularColor(Color.RED);
-
-        final PhongMaterial greenMaterial = new PhongMaterial();
-        greenMaterial.setDiffuseColor(Color.DARKGREEN);
-        greenMaterial.setSpecularColor(Color.GREEN);
-
-        final PhongMaterial blueMaterial = new PhongMaterial();
-        blueMaterial.setDiffuseColor(Color.DARKBLUE);
-        blueMaterial.setSpecularColor(Color.BLUE);
-
-        final Box xAxis = new Box(240.0, 1, 1);
-        final Box yAxis = new Box(1, 240.0, 1);
-        final Box zAxis = new Box(1, 1, 240.0);
-
-        xAxis.setMaterial(redMaterial);
-        yAxis.setMaterial(greenMaterial);
-        zAxis.setMaterial(blueMaterial);
-
-        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
-        world.getChildren().addAll(axisGroup);
+        return new Group(meshView);
     }
 
 
 
-    private void handleMouse(Scene scene, final Node root) {
-        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent me) {
-                mousePosX = me.getSceneX();
-                mousePosY = me.getSceneY();
-                mouseOldX = me.getSceneX();
-                mouseOldY = me.getSceneY();
-            }
-        });
-        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent me) {
-                mouseOldX = mousePosX;
-                mouseOldY = mousePosY;
-                mousePosX = me.getSceneX();
-                mousePosY = me.getSceneY();
-                mouseDeltaX = (mousePosX - mouseOldX);
-                mouseDeltaY = (mousePosY - mouseOldY);
 
-                double modifier = 1.0;
-
-                if (me.isControlDown()) {
-                    modifier = CONTROL_MULTIPLIER;
-                }
-                if (me.isShiftDown()) {
-                    modifier = SHIFT_MULTIPLIER;
-                }
-                if (me.isPrimaryButtonDown()) {
-                    cameraXform.ry.setAngle(cameraXform.ry.getAngle() - mouseDeltaX*MOUSE_SPEED*modifier*ROTATION_SPEED);
-                    cameraXform.rx.setAngle(cameraXform.rx.getAngle() + mouseDeltaY*MOUSE_SPEED*modifier*ROTATION_SPEED);
-                }
-                else if (me.isSecondaryButtonDown()) {
-                    double z = camera.getTranslateZ();
-                    double newZ = z + mouseDeltaX*MOUSE_SPEED*modifier;
-                    camera.setTranslateZ(newZ);
-                }
-                else if (me.isMiddleButtonDown()) {
-                    cameraXform2.t.setX(cameraXform2.t.getX() + mouseDeltaX*MOUSE_SPEED*modifier*TRACK_SPEED);
-                    cameraXform2.t.setY(cameraXform2.t.getY() + mouseDeltaY*MOUSE_SPEED*modifier*TRACK_SPEED);
-                }
-            }
-        });
-    }
-
-    private void handleKeyboard(Scene scene, final Node root) {
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case Z:
-                        cameraXform2.t.setX(0.0);
-                        cameraXform2.t.setY(0.0);
-                        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
-                        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
-                        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
-                        break;
-                    case X:
-                        axisGroup.setVisible(!axisGroup.isVisible());
-                        break;
-                    case V:
-                        moleculeGroup.setVisible(!moleculeGroup.isVisible());
-                        break;
-                }
-            }
-        });
+    private SubScene createScene3D(Group group) {
+        SubScene scene3d = new SubScene(group, VIEWPORT_SIZE, VIEWPORT_SIZE * 9.0/16, true, SceneAntialiasing.DISABLED);
+        scene3d.setFill(Color.rgb(10, 10, 40));
+        scene3d.setCamera(new PerspectiveCamera());
+        return scene3d;
     }
 
 
-    private void buildMolecule() {
-        //======================================================================
-        // THIS IS THE IMPORTANT MATERIAL FOR THE TUTORIAL
-        //======================================================================
 
-        final PhongMaterial redMaterial = new PhongMaterial();
-        redMaterial.setDiffuseColor(Color.DARKRED);
-        redMaterial.setSpecularColor(Color.RED);
 
-        final PhongMaterial whiteMaterial = new PhongMaterial();
-        whiteMaterial.setDiffuseColor(Color.WHITE);
-        whiteMaterial.setSpecularColor(Color.LIGHTBLUE);
-
-        final PhongMaterial greyMaterial = new PhongMaterial();
-        greyMaterial.setDiffuseColor(Color.DARKGREY);
-        greyMaterial.setSpecularColor(Color.GREY);
-
-        // Molecule Hierarchy
-        // [*] moleculeXform
-        //     [*] oxygenXform
-        //         [*] oxygenSphere
-        //     [*] hydrogen1SideXform
-        //         [*] hydrogen1Xform
-        //             [*] hydrogen1Sphere
-        //         [*] bond1Cylinder
-        //     [*] hydrogen2SideXform
-        //         [*] hydrogen2Xform
-        //             [*] hydrogen2Sphere
-        //         [*] bond2Cylinder
-        Xform moleculeXform = new Xform();
-        Xform oxygenXform = new Xform();
-        Xform hydrogen1SideXform = new Xform();
-        Xform hydrogen1Xform = new Xform();
-        Xform hydrogen2SideXform = new Xform();
-        Xform hydrogen2Xform = new Xform();
-
-        Sphere oxygenSphere = new Sphere(40.0);
-        oxygenSphere.setMaterial(redMaterial);
-
-        Sphere hydrogen1Sphere = new Sphere(30.0);
-        hydrogen1Sphere.setMaterial(whiteMaterial);
-        hydrogen1Sphere.setTranslateX(0.0);
-
-        Sphere hydrogen2Sphere = new Sphere(30.0);
-        hydrogen2Sphere.setMaterial(whiteMaterial);
-        hydrogen2Sphere.setTranslateZ(0.0);
-
-        Cylinder bond1Cylinder = new Cylinder(5, 100);
-        bond1Cylinder.setMaterial(greyMaterial);
-        bond1Cylinder.setTranslateX(50.0);
-        bond1Cylinder.setRotationAxis(Rotate.Z_AXIS);
-        bond1Cylinder.setRotate(90.0);
-
-        Cylinder bond2Cylinder = new Cylinder(5, 100);
-        bond2Cylinder.setMaterial(greyMaterial);
-        bond2Cylinder.setTranslateX(50.0);
-        bond2Cylinder.setRotationAxis(Rotate.Z_AXIS);
-        bond2Cylinder.setRotate(90.0);
-
-        moleculeXform.getChildren().add(oxygenXform);
-        moleculeXform.getChildren().add(hydrogen1SideXform);
-        moleculeXform.getChildren().add(hydrogen2SideXform);
-        oxygenXform.getChildren().add(oxygenSphere);
-        hydrogen1SideXform.getChildren().add(hydrogen1Xform);
-        hydrogen2SideXform.getChildren().add(hydrogen2Xform);
-        hydrogen1Xform.getChildren().add(hydrogen1Sphere);
-        hydrogen2Xform.getChildren().add(hydrogen2Sphere);
-        hydrogen1SideXform.getChildren().add(bond1Cylinder);
-        hydrogen2SideXform.getChildren().add(bond2Cylinder);
-
-        hydrogen1Xform.setTx(100.0);
-        hydrogen2Xform.setTx(100.0);
-        hydrogen2SideXform.setRotateY(HYDROGEN_ANGLE);
-
-        moleculeGroup.getChildren().add(moleculeXform);
-
-        world.getChildren().addAll(moleculeGroup);
-    }
 
     @Override public void start(Stage primaryStage) throws Exception{
-        /*
-        buildScene();
-        buildCamera();
-        buildAxes();
+
+       // Group group = buildScene();
+        //((Pane) scene2.getRoot()).getChildren().add(scene1.getRoot());
+        /**
         Parent form = FXMLLoader.load(getClass().getResource("form.fxml"));
         Xform test = new Xform();
         primaryStage.setTitle("DELVIS");
-       // Scene scene = new Scene(root, 1024, 768, true);
-
-        Scene hud = new Scene(form, 1024, 768, true);
-
-        Scene screen = new Scene(root, 1024, 768, true);
-        screen.setFill(Color.GRAY);
-        //subScene.setFill(Color.GREY);
-        primaryStage.setScene(screen);
-
-        //primaryStage.getScene().getStylesheets().add("futureCustomTheme.css");
-        primaryStage.getIcons().add(new Image("icon.png"));
-        primaryStage.setResizable(true);
-        buildMolecule();
-        //Canvas overlay = new Canvas(600,600);
+        StackPane root = new StackPane();
+        AnchorPane testy = new AnchorPane();
+        testy.getChildren().addAll(form);
 
 
-        handleKeyboard(screen, world);
-        handleMouse(screen, world);
-        //  MyController controller = loader.getController();
-        // controller.setStage(this.stage);
+        Scene scene = new Scene (root, 1024, 768, false);
+        SubScene subScene = new SubScene(testy, 1024, 768, true, SceneAntialiasing.DISABLED);
+        root.getChildren().addAll(subScene);
+        root.getChildren().addAll(getOverlay());
+        primaryStage.setScene(scene);
         primaryStage.show();
-        screen.setCamera(camera);
-        root.getChildren().add(form);
-            */
+        primaryStage.getIcons().addAll(new Image("icon.png"));
+        */
 
 
+        /**
+        WebView webView = new WebView();
+        webView.getEngine().load("http://www.vk.com");
+        StackPane root = new StackPane();
+        root.getChildren().addAll(webView, getOverlay());
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+        /*/
 
     }
 
+    private Pane getOverlay() {
+        StackPane p = new StackPane();
+        Button test = new Button();
+        test.textProperty().setValue("HELLO");
+        Rectangle r = RectangleBuilder.create()
+                .height(100).width(100)
+                .arcHeight(40).arcWidth(40)
+                .stroke(Color.RED)
+                .fill(Color.web("red", 0.1))
+                .build();
 
-   static public void generateMathFile(String filename, Point2D[] array) throws IOException {
+        Text txt= TextBuilder.create().text("OVERLAY")
+                .font(Font.font("Arial", FontWeight.BOLD, 18))
+                .fill(Color.BLUE)
+                .build();
+        p.getChildren().addAll(r, txt, test);
+        return p;
+    }
+
+
+   static public void generateMathFile(String filename, Vertex[] array, Vertex massCenter, Vertex nearest) throws IOException {
 
         String dir = System.getProperty("user.dir");
         String localname = dir +  "\\" + filename + ".m";
@@ -303,30 +180,60 @@ public class Main extends Application {
             wrt.write(Double.toString(array[i].getY())+ ",");
         }
         wrt.write("],  'o', 'Color', 'R'); \n");
-        wrt.write("hold on \n");
+        wrt.write("hold all; \n");
+        wrt.write("plot( " + massCenter.getX() + " , " + massCenter.getY() + ", 'o', 'Color', 'B', 'Linewidth', 3); \n");
+       wrt.write("plot( " + nearest.getX() + " , " + nearest.getY() + ", 'o', 'Color', 'G', 'Linewidth', 3); \n");
+        wrt.flush();
+    }
+
+
+    static public void generateTriangulationFile(String filename,Triangulation test) throws IOException {
+
+        String dir = System.getProperty("user.dir");
+        String localname = dir +  "\\" + filename + ".m";
+        File out = new File(localname);
+        FileWriter wrt = new FileWriter(out);
+
+        LinkedList<HalfEdge> list = test.getEdgesList();
+        for (int i = 0; i < list.size(); ++i){
+            wrt.write("plot( [" + list.get(i).getStart().getX() + " , " + list.get(i).getEnd().getX()  + "], ["  + list.get(i).getStart().getY() + " , " + list.get(i).getEnd().getY() + " ],  'Color', 'G', 'Linewidth', 2); \n");
+            wrt.write("hold all; \n");
+        }
+
         wrt.flush();
     }
 
 
     public static void main(String[] args){
-    	Point2D array[] = new Point2D[10000];
-
-        Random r = new Random();
-        Random z = new Random();
-        Random k = new Random();
-
-        for (int i = 0; i < 10000; ++i){
-            array[i] = new Point2D(r.nextDouble() * z.nextInt() * k.nextDouble() * (-1),  r.nextDouble() * z.nextInt()* k.nextDouble() * (-1));
+        //launch(args); //return;
+        double v1, v2;
+        Random a = new Random();
+        Random b = new Random();
+        Random c = new Random();
+        Random g = new Random();
+        Vertex array[] = new Vertex[1000];
+        for (int i = 0; i < 1000; ++i){
+            array[i] = new Vertex(a.nextDouble() * b.nextInt(), c.nextDouble() * g.nextInt());
         }
 
-            DeloneTriangulation localTriangualtion = new DeloneTriangulation(array);
+        Triangulation test = new Triangulation(array);
 
+        //Vertex massCenter = test.getMassCenter();
+       // Vertex nearest = test.getNearest();
+        /**
+        try {
+            generateMathFile("Denis", array, massCenter, nearest);
+        } catch (IOException error){
 
+        }
+         */
 
+        try {
 
+            generateTriangulationFile("testtests", test);
+        } catch (IOException error){
 
-
-       //launch(args); //return;
+        }
     }
 
 
--- src/Executable/formController.java
+++ src/Executable/formController.java
@@ -4,46 +4,142 @@ package Executable;
  * Created by emxot_000 on 12.05.2015.
  */
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Cursor;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.ScatterChart;
-import javafx.scene.chart.XYChart;
+import javafx.geometry.Point3D;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.*;
-import javafx.scene.image.*;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBoxBuilder;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
-import javafx.stage.*;
-
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Sphere;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class formController implements Initializable{
+    @FXML ImageView applicationLogo;
+    @FXML Button openFileButton;
+    @FXML AnchorPane mainPane;
+    //@FXML AnchorPane geometryView;
+    final Xform world = new Xform();
+    final Xform axisGroup = new Xform();
+    final Xform cameraXform = new Xform();
+    final Xform cameraXform2 = new Xform();
+    final Xform cameraXform3 = new Xform();
+    final Xform moleculeGroup = new Xform();
+    final double cameraDistance = 350;
+    final PerspectiveCamera camera = new PerspectiveCamera(true);
+
+
+    private Timeline timeline;
+    boolean timelinePlaying = false;
+    double ONE_FRAME = 1.0 / 24.0;
+    double DELTA_MULTIPLIER = 200.0;
+    double CONTROL_MULTIPLIER = 0.1;
+    double SHIFT_MULTIPLIER = 0.1;
+    double ALT_MULTIPLIER = 0.5;
+    double mousePosX;
+    double mousePosY;
+    double mouseOldX;
+    double mouseOldY;
+    double mouseDeltaX;
+    double mouseDeltaY;
+    int value = 90;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        buildScene();
+        buildCamera();
+        buildModels(90);
+        buildAxes();
 
+
+    }
+
+
+    private void buildScene() {
+        //geometryView.getChildren().add(world);
+    }
+
+    private void buildCamera() {
+
+        //geometryView.getChildren().add(cameraXform);
+
+        cameraXform.getChildren().add(cameraXform2);
+        cameraXform2.getChildren().add(cameraXform3);
+        cameraXform3.getChildren().add(camera);
+        cameraXform3.setRotateZ(180.0);
+
+        camera.setNearClip(0.1);
+        camera.setFarClip(10000.0);
+        camera.setTranslateZ(-cameraDistance);
+        cameraXform.ry.setAngle(320.0);
+        cameraXform.rx.setAngle(40);
+    }
+
+
+
+    private void buildAxes(){
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+
+        final Box xAxis = new Box(240.0, 1, 1);
+        final Box yAxis = new Box(1, 240.0, 1);
+        final Box zAxis = new Box(1, 1, 240.0);
+
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        world.getChildren().addAll(axisGroup);
+    }
+
+
+    private void buildModels(int value){
+        Xform moleculeXform = new Xform();
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+        moleculeGroup.getChildren().add(moleculeXform);
+        Sphere oxygenSphere = new Sphere(value);
+        oxygenSphere.setMaterial(redMaterial);
+        moleculeXform.getChildren().addAll(oxygenSphere);
+        world.getChildren().addAll(moleculeGroup);
+    }
+
+    private void buildM(int value){
+        Xform moleculeXform = new Xform();
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+        moleculeGroup.getChildren().add(moleculeXform);
+        Sphere oxygenSphere = new Sphere(value);
+        oxygenSphere.setMaterial(redMaterial);
+        moleculeXform.getChildren().addAll(oxygenSphere);
+        //world.getChildren().addAll(moleculeGroup);
+    }
+
+
+    public void keyboardTest(KeyEvent e){
+        if (e.isControlDown()){
+            value += 10;
+            buildM(value);
+        }
     }
 
 }
