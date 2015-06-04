33eaf72352646fc3818620c20b5f49f0db4325e7
Daniil Demidenko <danilka95v@gmail.com>
2015-06-04 23:13:26 +1000

    fix

--- src/Executable/formController.java
+++ src/Executable/formController.java
@@ -301,7 +301,7 @@ public class formController implements Initializable{
             localLine.setEndY(localList.get(i).getEdge().getNext().getEnd().getY());
             mainPane.getChildren().add(localLine);
 
-            /**
+            
             Line localLine1 = new Line();
             localLine1.setStroke(Color.rgb(213, 132, 36));
             localLine1.setStrokeWidth(2);
@@ -311,7 +311,7 @@ public class formController implements Initializable{
             localLine1.setEndX(localList.get(i).getEdge().getPrev().getEnd().getX());
             localLine1.setEndY(localList.get(i).getEdge().getPrev().getStart().getY());
             mainPane.getChildren().add(localLine1);
-        */
+
 
             Line localLine2 = new Line();
             localLine2.setStroke(Color.rgb(213, 65, 36));
