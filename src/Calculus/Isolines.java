6d84f4ffc0f1a23d158bcfdd15b1966ad7ae6b08
denSAKH <balanev.di@gmail.com>
2015-06-15 13:57:45 +1000

    IsolinesBeginning

--- src/Calculus/Isolines.java
+++ src/Calculus/Isolines.java
@@ -1,5 +1,23 @@
 package Calculus;
 
+import java.util.LinkedList;
+import DataTypes.*;
+
+
 public class Isolines {
+	private LinkedList<LinkedList<HalfEdge>> closed = null;
+	private LinkedList<LinkedList<HalfEdge>> unclosed = null;
 	
+	public Isolines(LinkedList<HalfEdge> edges, LinkedList<Face> faces, int n){
+		boolean belongs[] = new boolean[edges.size()];
+		
+	}
+	
+	public LinkedList<LinkedList<HalfEdge>> getClosedIsolines(){
+		return closed;
+	}
+	
+	public LinkedList<LinkedList<HalfEdge>> getUnclosedIsolines(){
+		return unclosed;
+	}
 }
